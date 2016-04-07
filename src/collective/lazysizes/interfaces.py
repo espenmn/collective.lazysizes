bd1c27624c206d66ce71ad10ad92369e9a6f5ec9
Espen Moe-Nilssen <espen@medialog.no>
2016-04-07 18:35:54 +0200

    typo

--- src/collective/lazysizes/interfaces.py
+++ src/collective/lazysizes/interfaces.py
@@ -24,11 +24,10 @@ class ILazySizesSettings(form.Schema):
             vocabulary=u'collective.lazysizes.ImageScales'),
     )
     
-	use_thumb = schema.Bool(
+    use_thumb = schema.Bool(
         title=_(u'Use thumb'),
         description=_(
             u'Use thumb image, or just use grey background.'),
-        required=True,
     )
 
     form.widget('css_class_blacklist', cols=25, rows=10)
